becc36679853ce18fccf5c5a707a4852822b2a03
Val <50930320+water96763@users.noreply.github.com>
2019-08-20 17:41:50 -0700

    Save update to liri

--- liri.js
+++ liri.js
@@ -4,15 +4,19 @@ var keys = require("./keys.js");
 var spotify =  new Spotify(keys.spotify);
 var axios = require("axios");
 var moment = require("moment");
+var fs = require('fs');
 
 // --------------SPOTIFY------------
 function searchSpotify() {
-if (process.argv.slice(3).join(" ") === "undefined") {
+if (!process.argv[3]) {
     spotify
     .search({ type: 'track,artist', query: "the sign base", limit: 1 })
     .then(function(response) {
-        console.log(response.tracks.items[0].artists[0].name);
-            console.log(response.tracks.items[0].album.artists[0].name);
+            console.log("+++++++++++++");
+            console.log("Artist: " +response.tracks.items[0].artists[0].name);
+            console.log("Song title: " +response.tracks.items[0].name);
+            console.log("Preview link: " +response.tracks.items[0].preview_url);
+            console.log("Album: " +response.tracks.items[0].album.name);
         })
         .catch(function(err) {
             console.log(err);
@@ -20,10 +24,13 @@ if (process.argv.slice(3).join(" ") === "undefined") {
     }
     else {
         spotify
-        .search({ type: 'track,artist', query: process.argv[3], limit: 1 })
+        .search({ type: 'track,artist', query: process.argv.slice(3).join(" "), limit: 1 })
         .then(function(response) {
-            console.log(response.tracks.items[0].artists[0].name);
-            console.log(response.tracks.items[0].album.artists[0].name);
+            console.log("+++++++++++++");
+            console.log("Artist: " +response.tracks.items[0].artists[0].name);
+            console.log("Song title: " +response.tracks.items[0].name);
+            console.log("Preview link: " +response.tracks.items[0].preview_url);
+            console.log("Album: " +response.tracks.items[0].album.name);
         })
         .catch(function(err) {
             console.log(err);
@@ -32,8 +39,9 @@ if (process.argv.slice(3).join(" ") === "undefined") {
 }
   // --------BANDS IN TOWN---------
 function searchBandsInTown() {
-        axios.get("https://rest.bandsintown.com/artists/" + process.argv[3] + "/events?app_id=codingbootcamp")
+        axios.get("https://rest.bandsintown.com/artists/" + process.argv.slice(3).join(" ") + "/events?app_id=codingbootcamp")
         .then(function(response) {
+            console.log("+++++++++++++");
             console.log("Venue: " + response.data[0].venue.name);
             console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country);
             console.log("Date: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
@@ -58,11 +66,11 @@ function searchBandsInTown() {
     //  ------------OMBD------------
 function searchOmdb() {
 
-    if (process.argv[3] === "undefined") {
+    if (!process.argv[3]) {
         axios.get("http://www.omdbapi.com/?i=tt0485947&y=&plot=short&apikey=trilogy")
         .then(
             function(response) {
-                console.log(response.data);
+                console.log("+++++++++++++");
                 console.log("Movie name: " + response.data.Title);
                 console.log("Release Year: " + response.data.Year);
                 console.log("IMDB rating: " + response.data.imdbRating);
@@ -92,6 +100,7 @@ function searchOmdb() {
     else {      
         axios.get("http://www.omdbapi.com/?t=" +process.argv.slice(3).join(" ") + "&y=&plot=short&apikey=trilogy")
             .then(function(response) {
+                console.log("+++++++++++++");
                 console.log("Movie name: " + response.data.Title);
                 console.log("Release Year: " + response.data.Year);
                 console.log("IMDB rating: " + response.data.imdbRating);
@@ -121,16 +130,29 @@ function searchOmdb() {
 }
         //   ------------Other----------------
 function searchFs() {
-            spotify
-            .search({ type: 'track', query: process.argv[3]})
-            .then(function(response) {
-                console.log(response);
-            })
-            .catch(function(err) {
-                console.log(err);
-            });
+        fs.readFile("random.txt", "utf8", function(err, data) {
+            if (err) throw err;
+
+            let dataArray = data.split(",");
+                console.log(dataArray[1]);
+                process.argv[3] = dataArray[1];
+                searchSpotify();
+            });        
         }
-    
+
+    function writeTextFile() {   
+        fs.writeFile('consolefile.txt', console.log, function (err) {
+        if (err) throw err;
+        console.log("Saved");
+});
+}
+
+function appendTextFile() {
+    fs.appendFile('consolefile.txt', console.log, function (err) {
+    if (err) throw err;
+});
+}
+
 if (process.argv[2] === "spotify-this-song") {
     searchSpotify();
 }
@@ -146,3 +168,5 @@ else if (process.argv[2] === "do-what-it-says") {
 else {
     console.log("Please choose something to do");
 }
+writeTextFile();
+appendTextFile();
